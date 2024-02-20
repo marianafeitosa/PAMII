e812d75cc49b5a56bf669abf82881701dfab38fb
mar¡ <99839074+marianafeitosa@users.noreply.github.com>
2024-02-19 23:59:15 -0300

    Add files via upload

---
+++ EXERCICIOS KOTLIN/EXERCICIO5/src/Main.kt
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package fundamentos
+fun main(args: Array<String>) {
+    val aprovados = listOf("João", "Maria", "Pedro")
+    print("O primeiro colocado foi ${aprovados[0]}")
+
+
+}
