90282ba749f4f49da5cc6cd8d254bdba239f562d
mar¡ <99839074+marianafeitosa@users.noreply.github.com>
2024-02-20 00:16:54 -0300

    Add files via upload

---
+++ EXERCICIOS KOTLIN/EXERCICIO8/src/Main.kt
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package fundamentos
+fun main(args: Array<String>) {
+    val nota : Double = 8.3
+    if(nota>= 7.0){ //Utilizando a estrutura if
+        println("Aprovado")
+    }
+
+}
