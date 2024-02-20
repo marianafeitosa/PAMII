040e65ad3603675f48f63baac49f1f0e8c37a40c
mar¡ <99839074+marianafeitosa@users.noreply.github.com>
2024-02-19 23:47:54 -0300

    Add files via upload

---
+++ EXERCICIOS KOTLIN/EXERCICIO3/src/Main.kt
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package fundamentos
+fun main(args: Array<String>) {
+    imprimirSoma(4 ,5)
+
+}
+fun imprimirSoma(a: Int, b:Int){
+    println(a+b)
+}
