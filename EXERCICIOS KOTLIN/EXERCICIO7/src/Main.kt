528af3479ff81937feda386db96b6baf3dd9a1aa
mar¡ <99839074+marianafeitosa@users.noreply.github.com>
2024-02-20 00:13:45 -0300

    Add files via upload

---
+++ EXERCICIOS KOTLIN/EXERCICIO7/src/Main.kt
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package fundamentos
+fun main(args: Array<String>) {
+    var a: Int = 33.dec()
+    var b: String = a.toString()  //Notação ponto
+    println("Int: $a")
+    println("Primeiro char da string b é: ${b.first()}")
+
+
+}
