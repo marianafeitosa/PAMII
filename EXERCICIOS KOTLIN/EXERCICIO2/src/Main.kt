b3c6d62826c2f238251ac7e9c1afd8704e9fddc3
mar¡ <99839074+marianafeitosa@users.noreply.github.com>
2024-02-19 23:21:07 -0300

    Add files via upload

---
+++ EXERCICIOS KOTLIN/EXERCICIO2/src/Main.kt
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package fundamentos
+fun main(args: Array<String>) {
+    var a: Int
+    var b = 2 //Tipo inferido
+    a = 10
+    print(a+b)
+
+}
