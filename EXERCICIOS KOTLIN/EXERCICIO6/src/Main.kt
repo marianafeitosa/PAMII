f2e360cc1b71088747d7d4774bbf5108a2ae946d
mar¡ <99839074+marianafeitosa@users.noreply.github.com>
2024-02-20 00:07:45 -0300

    Add files via upload

---
+++ EXERCICIOS KOTLIN/EXERCICIO6/src/Main.kt
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package fundamentos
+fun main(args: Array<String>) {
+    val bomHumor = true
+    print("Hoje estou ${if (bomHumor) "feliz" else "chateado"}")
+
+}
